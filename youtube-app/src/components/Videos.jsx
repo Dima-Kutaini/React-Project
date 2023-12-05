3603c6cd5039c4a7497640090c0237cec6bfa0aa
Dima-Kutaini <deemaKutainy@gmail.com>
2023-12-05 22:01:07 +0100

    create videos channel

--- youtube-app/src/components/Videos.jsx
+++ youtube-app/src/components/Videos.jsx
@@ -14,7 +14,7 @@ function Videos({videos}) {
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
-          {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
+          {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
       ))}
     </Stack>
